510856172f190ec5934e5945b3caa84720351948
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-02-22 17:57:17 -0500

    create server and fetch route for static data

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: './index.html',
+    port: 5712,
+  },
+});
