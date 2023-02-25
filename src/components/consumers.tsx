0cec891f5445c13d142632fb67c29d2b36dac49c
Rebecca Kwong <kwong.rebe@gmail.com>
2023-02-25 15:49:24 -0500

    KAL-72: basic grid table formatting

--- src/components/consumers.tsx
+++ src/components/consumers.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ConsumersDisplay from './ConsumersDisplay';
+import {Typography} from '@mui/material';
 
 const Consumers = () => {
   return (
     <div className="wrapper">
       <div className="consumers-heading" data-testid="consumer-1">
-        <h3>Consumers</h3>
-        <input type="text" placeholder="Search" />
+        <Typography variant="h6">Consumer</Typography>
       </div>
       <div className="consumers-display">
         <ConsumersDisplay></ConsumersDisplay>
