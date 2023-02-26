d010a45672457a80c3afa6c9ffb8062a72255c4b
Julien Devlin <juliendevlin@gmail.com>
2023-02-26 18:38:59 -0500

    remove old directories and files

--- server/demo/index.tsx
+++
@@ -1,6 +0,0 @@
-import publishMessages from './producer';
-import consumeMessages from './consumer';
-
-// run the producer and consumer
-setInterval(publishMessages, 5000);
-setInterval(consumeMessages, 5000);
