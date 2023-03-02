cf41bb7794a787c40539ff8845d73895d9cae00c
Julien Devlin <juliendevlin@gmail.com>
2023-03-02 11:32:19 -0500

    reconvert jest-setup to ts

--- jest-setup.ts
+++ jest-setup.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// fetch polyfill for rtl tests
+import 'whatwg-fetch';
