f0d69f3f7010760ec2145053d7190ce1c7200bf8
ashleegaf <ashleegaf@gmail.com>
2023-03-06 13:43:03 -0500

    Update kafkaController to store connection details

--- src/server/controllers/kafkaController.ts
+++ src/server/controllers/kafkaController.ts
@@ -8,25 +8,31 @@ const kafkaController: controller = {};
 kafkaController.initiateKafka = async (req, res, next) => {
   const {clientId, brokers, ssl, sasl} = req.body;
 
+  // create objects with and w/o ssl and sasl
+  const noSasl = {clientId, brokers};
+  const withSasl = {clientId, brokers, ssl, sasl};
+
   try {
     if (!sasl) {
       // creating kafka instance on the exported controller object so it will be accessible elsewhere
-      kafkaController.kafka = new Kafka({
-        clientId,
-        brokers,
-      });
+      // kafkaController.kafka = new Kafka({
+      //   clientId,
+      //   brokers,
+      // });
+      kafkaController.kafka = new Kafka(noSasl);
     } else {
       console.log('connecting with sasl...');
-      kafkaController.kafka = new Kafka({
-        clientId,
-        brokers,
-        ssl,
-        sasl,
-      });
+      // kafkaController.kafka = new Kafka({
+      //   clientId,
+      //   brokers,
+      //   ssl,
+      //   sasl,
+      // });
+      kafkaController.kafka = new Kafka(withSasl);
     }
-    const testAdmin = kafkaController.kafka.admin();
-    await testAdmin.connect();
-    await testAdmin.disconnect();
+    const appAdmin = kafkaController.kafka.admin();
+    await appAdmin.connect();
+    await appAdmin.disconnect();
 
     return next();
   } catch (err) {
@@ -39,3 +45,8 @@ kafkaController.initiateKafka = async (req, res, next) => {
 };
 
 export default kafkaController;
+
+// {
+//   cluster1: {...noSasl, sasl: false},
+//   cluster1: {...Sasl, sasl: true},
+// }
