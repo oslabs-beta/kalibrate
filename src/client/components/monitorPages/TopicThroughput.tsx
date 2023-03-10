0b8ac9c29497e0e11489f2729fd05a888a78e8a1
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-03-10 18:27:56 -0500

    msgs/second line graphs render dynamically

--- src/client/components/monitorPages/TopicThroughput.tsx
+++ src/client/components/monitorPages/TopicThroughput.tsx
@@ -21,13 +21,19 @@ const TopicThroughput = props => {
   console.log('TSD: ', timeSeriesData);
 
   const [topicThroughputData, setTopicThroughPutData] = useState<throughputData>({});
-  const [xSeries, setXSeries] = useState<number[]>([]);
+  const [xSeries, setXSeries] = useState<string[]>([]);
+  const [xScope, setxScope] = useState<number>(10);
 
-  // on mount, create topic arrays in throughput data object
+  // on mount, create topic arrays in throughput data object, and populate xSeries with empty strings
   useEffect(() => {
+    const blankArray = new Array(xScope);
+    blankArray.fill('');
+    setXSeries(blankArray);
     const newTopicObj = {};
     for (const el in timeSeriesData[0].topicOffsets) {
       newTopicObj[el] = [];
+      newTopicObj[el].length = xScope;
+      newTopicObj[el].fill(null);
       setTopicThroughPutData(newTopicObj);
       console.log('set throughput data obj: ', newTopicObj);
     }
@@ -42,7 +48,9 @@ const TopicThroughput = props => {
     const previous = timeSeriesData[timeSeriesData.length - 2];
     // add time to x-axis data
     const newTime = [...xSeries];
-    newTime.push(current.time);
+    const time = new Date(current.time).toLocaleTimeString();
+    newTime.push(time);
+    if (newTime.length > xScope) newTime.shift();
     setXSeries(newTime);
     // copy throughput data object to change before updating state
     const newData = JSON.parse(JSON.stringify(topicThroughputData));
@@ -50,6 +58,8 @@ const TopicThroughput = props => {
       // push y-axis data to the appropriate array and update state
       if (newData.hasOwnProperty(el)) {
         newData[el].push(current.topicOffsets[el] - previous.topicOffsets[el]);
+        // adjust data to fit desired amount of most recent data pts on graph
+        if (newData[el].length > xScope) newData[el].shift();
       } else {
         newData[el] = [];
       }
@@ -58,7 +68,10 @@ const TopicThroughput = props => {
     // using the last element of the array as the dependency guarantees updates both while the array gets longer and after it reaches max length of 50
   }, [timeSeriesData[timeSeriesData.length - 1]]);
 
+  // global chart plugins - maybe move during refactoring
   ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+  const labels = xSeries; // x-axis labels are timestamps from state
 
   const options = {
     responsive: true,
@@ -68,28 +81,31 @@ const TopicThroughput = props => {
       },
       title: {
         display: true,
-        text: 'Chart.js Line Chart',
+        text: 'Topic Offsets',
       },
     },
   };
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  // add x-axis size to state??
 
   const data = {
     labels,
     datasets: [
       {
         label: 'Dataset 1',
-        data: topicThroughputData['unfulfilled'],
+        data: topicThroughputData['payments'],
+        spanGaps: true,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        hidden: false,
       },
-      // {
-      //   label: 'Dataset 2',
-      //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      //   borderColor: 'rgb(53, 162, 235)',
-      //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      // },
+      {
+        label: 'Dataset 2',
+        data: topicThroughputData['__consumer_offsets'],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        hidden: true,
+      },
     ],
   };
 
