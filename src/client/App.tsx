662cfca24273f772e9f981eb2c832794d83b0521
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-03-12 22:13:25 -0400

    Revert "begin to migrate poll to separate file"
    This reverts commit 0ba89c0ff71e5a2a87996138b010f0f0f36a4065.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -30,7 +30,6 @@ import {ColorModeContext, useMode} from './theme';
 import {ThemeProvider, CssBaseline, Snackbar, Alert} from '@mui/material';
 import {GroupTopic, newPollType, storedClient} from './types';
 import {not} from 'ip';
-import poll from './util/poll';
 import TrafficAndHealthGraphs from './components/monitorPages/TrafficAndHealthGraphs';
 
 function App() {
@@ -128,17 +127,7 @@ function App() {
           setIsConnectionLoading(false);
 
           //set interval for polling
-          const interval: number = window.setInterval(
-            poll(
-              connectedClient,
-              setConnectedClusterData,
-              timeSeriesData,
-              xSeries,
-              setXSeries,
-              addTimeSeries
-            ),
-            pollInterval * 1000
-          );
+          const interval: number = window.setInterval(poll, pollInterval * 1000);
           setCurrentPollInterval(interval);
         })
         .catch(err => {
@@ -154,6 +143,110 @@ function App() {
       };
     }
   }, [connectedClient]);
+
+  // long poll to connected kafka instance for data
+  // since we have to get data from kafka with KJS I'm not sure websockets do anything but add an intermediate step
+  // possible todo: modularize poll into a different file
+  const poll = () => {
+    fetch(`/api/data/${connectedClient}`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newPoll: newPollType = {
+          cluster: connectedClient,
+        };
+        newPoll.time = Date.now();
+        setConnectedClusterData(data);
+
+        // process data from connected cluster into more graph-ready form:
+        // can be further processed if we want, maybe in the graph component
+
+        // percent of groups by status
+        let stable = 0,
+          empty = 0;
+        for (const el of data.groupData) {
+          if (el.state === 'Stable') stable++;
+          if (el.state === 'Empty') empty++;
+        }
+        newPoll.groupStatus = {
+          total: data.groupData.length,
+          stable,
+          empty,
+          other: data.groupData.length - stable - empty,
+        };
+
+        // topic replica status (percentage in sync)
+        newPoll.topicReplicaStatus = {};
+        for (const el of data.topicData.topics) {
+          let replicas = 0;
+          let isr = 0;
+          for (const p of el.partitions) {
+            replicas += p.replicas.length;
+            isr += p.isr.length;
+          }
+
+          newPoll.topicReplicaStatus[el.name] = Math.round((isr / replicas) * 100);
+        }
+
+        // count of offsets by topic
+        newPoll.topicOffsets = {};
+        newPoll.topicThroughputs = {};
+        if (data.topicData.topics.length) {
+          for (const t of data.topicData.topics) {
+            newPoll.topicOffsets[t.name] = t.offsets.reduce(
+              (acc: number, curr: OffsetCollection) => {
+                return acc + Number(curr.offset);
+              },
+              0
+            );
+
+            // rate of throughtput by topic since last poll
+
+            if (timeSeriesData.at(-1)) {
+              const previous: newPollType = timeSeriesData.at(-1);
+              newPoll.topicThroughputs[t.name] =
+                (newPoll.topicOffsets[t.name] - previous.topicOffsets[t.name]) /
+                ((newPoll.time - previous.time) / 1000);
+            }
+          }
+        }
+
+        // count of offsets by group
+        newPoll.groupOffsets = {};
+        newPoll.groupThroughputs = {};
+        for (const g in data.groupOffsets) {
+          //const groupName = g;
+          let sum = 0;
+          data.groupOffsets[g].forEach((el: GroupTopic) => {
+            el.partitions.forEach(p => {
+              sum += Number(p.offset);
+            });
+          });
+          newPoll.groupOffsets[g] = sum;
+
+          // rate of throughtput by group since last poll
+
+          if (timeSeriesData.at(-1)) {
+            const previous: newPollType = timeSeriesData.at(-1);
+            newPoll.groupThroughputs[g] =
+              (newPoll.groupOffsets[g] - previous.groupOffsets[g]) /
+              ((newPoll.time - previous.time) / 1000);
+          }
+        }
+        addTimeSeries(newPoll);
+        // add timeseriesdata to state so we can drill it/use it for graphing
+        // limit to 50 columns for performance, for now
+        //const newTimeSeriesData = timeSeriesData;
+        //newTimeSeriesData.push(newPoll);
+        //if (newTimeSeriesData.length > 50) newTimeSeriesData.shift();
+        // to help while figuring out graphs: this is the snapshot of graphable data, added to every <interval> seconds
+      })
+      .catch(err => console.log(`Error polling data: ${err}`));
+  };
 
   const addTimeSeries = (newPoll: newPollType) => {
     // if consumer group status alerts are enabled, check notification and notify if applicable
